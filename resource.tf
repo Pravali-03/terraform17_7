bef40ca33b9091afc30268377676c484f9489d19
Pravali-03 <pravallika9603@gmail.com>
2025-07-17 04:37:48 +0000

    commit2

--- resource.tf
+++ resource.tf
@@ -1,12 +1,5 @@
-resource "azurerm_resource_group" "storeterra" {
-    name     = "example-resources"
+resource "azurerm_resource_group" "terraazure" {
+    name     = "terraazure"
     location = "East US"
 }
  
-resource "azurerm_storage_account" "saterra" {
-    name                     = "terrastorage238ppchuri"
-    resource_group_name      = azurerm_resource_group.storeterra.name
-    location                 = azurerm_resource_group.storeterra.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-}
